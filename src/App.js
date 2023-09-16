a96a21d26c40a923856d1a0abffea51fd10b7dbf
htripathi12 <tripathh@purdue.edu>
2023-09-16 13:28:35 -0400

    Add button test

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <button onClick={sayHello}>Improve Your Profile!</button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +20,10 @@ function App() {
       </header>
     </div>
   );
+}
+
+function sayHello() {
+  alert('Insert Link Here When Ready');
 }
 
 export default App;
