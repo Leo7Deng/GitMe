162f13a65817e898e7262e98566fafe4f6289437
htripathi12 <tripathh@purdue.edu>
2023-09-16 18:03:28 -0400

    I literally want to die

---
+++ src/ResumeLink.js
@@ -0,0 +1,17 @@
+import './ResumeLink.css';
+
+function resumeReview(){
+    return(
+        <div className="App">
+            <body>
+                <form className="form"> 
+                    <label for="username">Username: </label>
+                    <input type="text" id="fname" name="fname"></input>
+                </form>
+            </body>
+        </div>
+        
+    );
+}
+
+export default resumeReview;
