e8bf9ec39d13ef337e3c44acda6c69d22f475826
operlaston <operlaston21@gmail.com>
2023-09-16 17:09:26 -0400

    created second block and started third

--- src/App.js
+++ src/App.js
@@ -5,14 +5,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet"/>
         <h1 class='head'>GitMe</h1>
         <h2 class='head'>An AI improvement application</h2>
       </header>
       <body>
         <div id="first-block">
-          <p class='text'>Improve Your Profile</p>
+          <p class='leftText'>Improve Your Profile</p>
           <img src = 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png'
-          alt = 'GitHub Logo' id='gHLogo'></img>
+          alt = 'GitHub Logo' id='ghLogo'/>
+        </div>
+        <div id="second-block">
+          <img src='https://cdn.iconscout.com/icon/free/png-512/free-readme-3627908-3029134.png?f=avif&w=512'
+          alt='readme icon' id='rmIcon'/>
+          <p class='rightText'>Improve Your README Documentation</p>
+        </div>
+        <div id="third-block">
+          <p class='leftText'>Improve Your Resume</p>
         </div>
       </body>
     </div>
