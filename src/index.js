424f144b2f4aa68f728edbf835e7c433a239d674
htripathi12 <tripathh@purdue.edu>
2023-09-16 20:38:57 -0400

    added resumelink comment for future local hosting shiiiiet

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Link from './Link';
+//import ResumeLink from './ResumeLink';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
