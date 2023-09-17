842105c6123056787d58d7ab6e02f3deaba7663a
Leo Deng <leo7deng@gmail.com>
2023-09-16 23:18:38 -0400

    button linked to next page

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
 //import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home"
+import Link from "./Link"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet"/>
-        <div id="intro-block">
-            <h1 class='head'>GitMe</h1>
-            <h2 class='head'>An AI improvement application</h2>
-        </div>
-      </header>
-      <body>
-        <div id="first-block" class='blocks'>
-          <p class='leftText'>Improve Your Profile</p>
-          <img src = 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png'
-          alt = 'GitHub Logo' id='ghLogo'/>
-        </div>
-        <div id="second-block" class='blocks'>
-          <img src='https://cdn.iconscout.com/icon/free/png-512/free-readme-3521667-2945111.png?f=avif&w=512'
-          alt='readme icon' id='rmIcon'/>
-          <p class='rightText'>Improve Your README Documentation</p>
-        </div>
-        <div id="third-block" class='blocks'>
-          <p class='leftText'>Improve Your Resume</p>
-          <img src='https://cdn.iconscout.com/icon/free/png-512/free-resume-39-529571.png?f=avif&w=512'
-          alt='resume icon' id='resIcon'/>
-        </div>
-      </body>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Link" element={<Link />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-function sayHello() {
-  alert('Insert Link Here When Ready');
-}
-
 export default App;
+
+
+
